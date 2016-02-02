9ead7a6313d854a9c8f0a03d4bd981ab465afb72
tak kon <konwtak@gmail.com>
2016-02-02 23:25:40 +0800

    implement controller, add listener for button

--- src/main/java/warptrading/java/game/GameController.java
+++ src/main/java/warptrading/java/game/GameController.java
@@ -1,5 +1,8 @@
 package warptrading.java.game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GameController
 {
     GameModel model;
@@ -10,4 +13,96 @@ public class GameController
         this.model = model;
         this.view = view;
     }
+
+    /**
+     * update GUI base on result and choice
+     */
+    private void changeView()
+    {
+
+        view.repaint();
+    }
+
+    /**
+     * "Play Game" by setting Player and Computer Choice in Model
+     * @param model is our Game Logic
+     * @param playerChoice is player choice
+     */
+    private void playGame(GameModel model, GameModel.CHOICE playerChoice)
+    {
+        model.setUserChoice(playerChoice);
+        model.setComputerChoice(model.computerThink());
+    }
+
+    // listen to paper button
+    class PaperButtonController implements ActionListener {
+        /* (non-Javadoc)
+         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+         * Play Game and Change GUI base on result
+         */
+        @Override
+        public void actionPerformed(ActionEvent ac)
+        {
+            playGame(model,GameModel.CHOICE.PAPER);
+            changeView();
+        }
+    }
+
+    // listen to scissor button
+    class ScissorsButtonController implements ActionListener{
+        /* (non-Javadoc)
+         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+         * Play Game and Change GUI base on result
+         */
+        @Override
+        public void actionPerformed(ActionEvent ac)
+        {
+            playGame(model,GameModel.CHOICE.SCISSORS);
+            changeView();
+        }
+    }
+
+    //listen to rock button
+    class RockButtonController implements ActionListener {
+        /* (non-Javadoc)
+         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+         * Play Game and Change GUI base on result
+         */
+        @Override
+        public void actionPerformed(ActionEvent ac)
+        {
+            playGame(model,GameModel.CHOICE.ROCK);
+            changeView();
+        }
+    }
+
+    //listen to spock button
+    class SpockButtonController implements ActionListener {
+        /* (non-Javadoc)
+         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+         * Play Game and Change GUI base on result
+         */
+        @Override
+        public void actionPerformed(ActionEvent ac)
+        {
+            playGame(model,GameModel.CHOICE.SPOCK);
+            changeView();
+        }
+    }
+
+    //listen to lizard button
+    class LizardButtonController implements ActionListener {
+        /* (non-Javadoc)
+         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+         * Play Game and Change GUI base on result
+         */
+        @Override
+        public void actionPerformed(ActionEvent ac)
+        {
+            playGame(model,GameModel.CHOICE.LIZARD);
+            changeView();
+        }
+    }
+
+
 }
